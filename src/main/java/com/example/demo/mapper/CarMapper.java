e32e871d289b8777115b2815ddcb4f3aeac31e57
Khánh Nè <khanhtran0259@gmail.com>
2025-05-08 14:37:48 +0700

    add file CarMapper for handle response for GET action

--- src/main/java/com/example/demo/mapper/CarMapper.java
+++ src/main/java/com/example/demo/mapper/CarMapper.java
@@ -1,2 +1,30 @@
-package com.example.demo.mapper;public class CarMapper {
+package com.example.demo.mapper;
+
+import com.example.demo.DTO.CarResponse;
+import com.example.demo.model.Car;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Component
+public class CarMapper {
+    public CarResponse toResponse(Car car) {
+        CarResponse response = new CarResponse();
+        response.setId(car.getId());
+        response.setCarName(car.getCarName());
+        response.setMfg(car.getMfg());
+        response.setPrice(car.getPrice());
+        response.setOwner(car.getOwner());
+        response.setBrandName(car.getBrand().getName());
+        response.setBrandType(car.getBrand().getCarType());
+        response.setCreatedAt(car.getCreatedAt());
+        return response;
+    }
+    public List<CarResponse> toCarResponseList(List<Car> cars) {
+        return cars.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
+
+
 }
